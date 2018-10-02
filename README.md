2fb79da2d80d1bc307d4b2a0170c425e5045d701
iionly <iionly@gmx.de>
2018-10-02 17:32:48 +0200

    Updated to version 2.3.1; changes see CHANGES.txt

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-Favicon Override plugin for Elgg 2.X
-====================================
+Favicon Override plugin for Elgg 2.3 and newer Elgg 2.X
+=======================================================
 
-Latest Version: 2.0.0  
-Released: 2016-01-18  
+Latest Version: 2.3.1  
+Released: 2018-08-11  
 Contact: iionly@gmx.de  
 License: GNU General Public License version 2  
 Copyright: (C) iionly 2016
@@ -18,5 +18,5 @@ Installation
 ------------
 
 1. If you have a previous version of the Favicon Override plugin installed, disable the plugin first and then remove the favicon_override plugin folder from the mod directory completely (don't forget to save your favicon images from the "mod/favicon_override/graphics" directory),
-2. Copy the favicon_override plugin folder into the mod directory of your Elgg installation and place your favicon images (resolutions 16x16px, 32x32px, 64x64px and 128x128px) into the "mod/favicon_override/graphics" folder. Rename your images to the names of the existing placeholder images of the corresponding sizes,
+2. Copy the favicon_override plugin folder into the mod directory of your Elgg installation and place your favicon images (resolutions 16x16px, 32x32px, 64x64px and 128x128px in png, favicon.ico and favicon.svg) into the "mod/favicon_override/graphics" folder. Rename your images to the names of the existing placeholder images of the corresponding sizes and formats,
 3. Enable the plugin in the admin section of your site. For your favicon image to show up immediately in your browser you might need to flush the site cache of your Elgg site and also the browser cache. Later on your favicon should show up automatically for your site users once the possibly cached files have expired (e.g. after 24h).
--- start.php
+++ start.php
@@ -4,41 +4,61 @@ elgg_register_event_handler('init', 'system', 'favicon_override_init');
 
 function favicon_override_init() {
 	elgg_register_plugin_hook_handler('head', 'page', 'favicon_override_head');
+
+	elgg_unregister_page_handler('favicon.ico', '_elgg_favicon_page_handler');
+	elgg_register_page_handler('favicon.ico', 'favicon_override_page_handler');
 }
 
 function favicon_override_head($hook, $type, $data) {
-	$data['links']['apple-touch-icon'] = array(
+	$data['links']['apple-touch-icon'] = [
 		'rel' => 'apple-touch-icon',
 		'href' => elgg_get_simplecache_url('favicon_override/favicon-128.png'),
-	);
-	$data['links']['icon-ico'] = array(
+	];
+	$data['links']['icon-ico'] = [
 		'rel' => 'icon',
 		'href' => elgg_get_simplecache_url('favicon_override/favicon.ico'),
-	);
-	$data['links']['icon-16'] = array(
+	];
+	$data['links']['icon-vector'] = [
+		'rel' => 'icon',
+		'sizes' => '16x16 32x32 48x48 64x64 128x128',
+		'type' => 'image/svg+xml',
+		'href' => elgg_get_simplecache_url('favicon_override/favicon.svg'),
+	];
+	$data['links']['icon-16'] = [
 		'rel' => 'icon',
 		'sizes' => '16x16',
 		'type' => 'image/png',
 		'href' => elgg_get_simplecache_url('favicon_override/favicon-16.png'),
-	);
-	$data['links']['icon-32'] = array(
+	];
+	$data['links']['icon-32'] = [
 		'rel' => 'icon',
 		'sizes' => '32x32',
 		'type' => 'image/png',
 		'href' => elgg_get_simplecache_url('favicon_override/favicon-32.png'),
-	);
-	$data['links']['icon-64'] = array(
+	];
+	$data['links']['icon-64'] = [
 		'rel' => 'icon',
 		'sizes' => '64x64',
 		'type' => 'image/png',
 		'href' => elgg_get_simplecache_url('favicon_override/favicon-64.png'),
-	);
-	$data['links']['icon-128'] = array(
+	];
+	$data['links']['icon-128'] = [
 		'rel' => 'icon',
 		'sizes' => '128x128',
 		'type' => 'image/png',
 		'href' => elgg_get_simplecache_url('favicon_override/favicon-128.png'),
-	);
+	];
 
 	return $data;
+}
+
+function favicon_override_page_handler($segments) {
+	header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', strtotime("+1 week")), true);
+	header("Pragma: public", true);
+	header("Cache-Control: public", true);
+
+	header('Content-Type: image/x-icon');
+	echo elgg_view('favicon_override/favicon.ico');
+
+	return true;
 }
